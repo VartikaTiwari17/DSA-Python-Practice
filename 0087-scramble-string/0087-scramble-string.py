2371d59a3b54cd48be04b023067e7aff2ae60095
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2026-08-27 23:05:49 +0530

    Time: 17 ms (68.4%), Space: 13.1 MB (16.29%) - LeetHub

---
+++ 0087-scramble-string/0087-scramble-string.py
@@ -0,0 +1,35 @@
+class Solution:
+    def isScramble(self, s1, s2):
+        memo = {}
+
+        def solve(a, b):
+            if a == b:
+                return True
+
+            key = (a, b)
+
+            if key in memo:
+                return memo[key]
+
+            if sorted(a) != sorted(b):
+                memo[key] = False
+                return False
+
+            n = len(a)
+
+            for i in range(1, n):
+
+                # No swap
+                if solve(a[:i], b[:i]) and solve(a[i:], b[i:]):
+                    memo[key] = True
+                    return True
+
+                # Swap
+                if solve(a[:i], b[n-i:]) and solve(a[i:], b[:n-i]):
+                    memo[key] = True
+                    return True
+
+            memo[key] = False
+            return False
+
+        return solve(s1, s2)
